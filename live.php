2fc2b26156bb55dc80eebe85a0ef72f3f3d6e1d6
tsimohan <moaj257@gmail.com>
2022-09-01 19:29:02 +0530

    Update live.php

--- live.php
+++ live.php
@@ -20,7 +20,8 @@ echo "<br/><br/><br/>";
 if ($token != "" && @$_REQUEST["c"] != "") {
     $opts = ["http" => ["method" => "GET", "header" => "User-Agent: plaYtv/6.0.9 (Linux; Android 5.1.1) ExoPlayerLib/2.13.2"]];
     $cx = stream_context_create($opts);
-    $hs = file_get_contents(("https://jiotv.live.cdn.jio.com/" . $_REQUEST["c"] . "/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . ".m3u8" . $token), false, $cx);
+    $hs = file_get_contents(("https://jiotv.live.cdn.jio.com/" . urlencode($_REQUEST["c"]) . "/" . urlencode($_REQUEST["c"]) . "_" . urlencode($_REQUEST["q"]) . ".m3u8" . urlencode($token)), false, $cx);
+    // $hs = handle("https://jiotv.live.cdn.jio.com/" . $_REQUEST["c"] . "/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . ".m3u8" . $token);
     print_r($hs); echo "<br/><br/><br/>";
     $hs = @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . "-([^.]+\.)key/", 'stream.php?key=' . $_REQUEST["c"] . '/' . $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1key', $hs);
     $hs = @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . "-([^.]+\.)ts/", 'stream.php?ts=' . $_REQUEST["c"] . '/' . $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1ts', $hs);
@@ -29,5 +30,17 @@ if ($token != "" && @$_REQUEST["c"] != "") {
     print_r(file_get_contents('https://www.google.com', false, $cx));
     print $hs;
 }
+function handle($url) {
+    $curl_handle=curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, $url);
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+    $query = curl_exec($curl_handle);
+    curl_close($curl_handle);
+
+    return $query;
+}
+
 die();
 ?>
