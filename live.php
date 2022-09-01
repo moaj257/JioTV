d2b157cb95df7750fba76232d09648798390e656
tsimohan <moaj257@gmail.com>
2022-09-01 18:49:37 +0530

    Debuging ++

--- live.php
+++ live.php
@@ -7,17 +7,13 @@ require ('token.php');
 
 session_start();
 
-header("Content-Type: application/vnd.apple.mpegurl");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Expose-Headers: Content-Length,Content-Range");
-header("Access-Control-Allow-Headers: Range");
-header("Accept-Ranges: bytes");
+// header("Content-Type: application/vnd.apple.mpegurl");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Expose-Headers: Content-Length,Content-Range");
+// header("Access-Control-Allow-Headers: Range");
+// header("Accept-Ranges: bytes");
 
 $_SESSION["p"] = $token;
-
-print_r($token);
-print_r($_REQUEST);
-print_r($_SESSION);
 
 if ($token != "" && @$_REQUEST["c"] != "")
 {
@@ -25,19 +21,24 @@ if ($token != "" && @$_REQUEST["c"] != "")
     $opts = [
         "http" => [
             "method" => "GET",
-            "header" => "User-Agent: plaYtv/6.0.9 (Linux; Android 5.1.1) ExoPlayerLib/2.13.2" 
-    
+            "header" => "User-Agent: plaYtv/6.0.9 (Linux; Android 5.1.1) ExoPlayerLib/2.13.2"
         ]
     ];
 
     $cx = stream_context_create($opts);
+    print_r($cx); echo "<br/><br/><br/>";
 
     $hs = file_get_contents("https://jiotv.live.cdn.jio.com/" . $_REQUEST["c"] . "/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] . ".m3u8" . $token, false, $cx);
+    print_r($hs); echo "<br/><br/><br/>";
     $hs= @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] ."-([^.]+\.)key/", 'stream.php?key='  . $_REQUEST["c"] . '/' .   $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1key', $hs);
+    print_r($hs); echo "<br/><br/><br/>";
     $hs= @preg_replace("/" . $_REQUEST["c"] . "_" . $_REQUEST["q"] ."-([^.]+\.)ts/", 'stream.php?ts='  . $_REQUEST["c"] . '/' .   $_REQUEST["c"] . '_' . $_REQUEST["q"] . '-\1ts', $hs);
+    print_r($hs); echo "<br/><br/><br/>";
 
     $hs=str_replace("https://tv.media.jio.com/streams_live/" .  $_REQUEST["c"] . "/","",$hs);
+    print_r($hs); echo "<br/><br/><br/>";
     $hs = str_replace("https://tv.media.jio.com/streams_hotstar/" . $_REQUEST["c"] . "/","",$hs);
+    print_r($hs); echo "<br/><br/><br/>";
 
 
     print $hs;
